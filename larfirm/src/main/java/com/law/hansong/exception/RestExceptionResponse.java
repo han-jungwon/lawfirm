0333ee8496fbd41f930425eaabb0da0f34e39c89
SongMinJun <dkaskgkdua@gmail.com>
2020-11-08 22:26:52 +0900

    20.11.08 송민준
    1. restcontroller advice 구현
       (exception 처리)
    2. 유저정보 공통 추가
    3. hateoas 라이브러리 추가

---
+++ larfirm/src/main/java/com/law/hansong/exception/RestExceptionResponse.java
@@ -0,0 +1,16 @@
+package com.law.hansong.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestExceptionResponse {
+    private Date timestamp;
+    private String message;
+    private String detail;
+}

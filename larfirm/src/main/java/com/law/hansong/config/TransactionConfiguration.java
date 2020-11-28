16ee9c7a5422d377b61a1926e7ca33c16a96313c
SongMinJun <dkaskgkdua@gmail.com>
2020-11-29 00:29:24 +0900

    2020. 11. 29 송민준
    1. 트랜잭션 속성 일부 추가

--- larfirm/src/main/java/com/law/hansong/config/TransactionConfiguration.java
+++ larfirm/src/main/java/com/law/hansong/config/TransactionConfiguration.java
@@ -27,39 +27,44 @@ public class TransactionConfiguration implements TransactionManagementConfigurer
 
     @Bean(value = "txManager")
     public PlatformTransactionManager txManager(DataSource dataSource) {
-        log.info("txManager init...");
         DataSourceTransactionManager tm = new DataSourceTransactionManager(dataSource);
         return tm;
     }
     @Bean(name = "transactionInterceptor")
     public TransactionInterceptor transactionInterceptor(PlatformTransactionManager platformTransactionManager) {
         TransactionInterceptor transactionInterceptor = new TransactionInterceptor();
-        // 事物管理器
+
         transactionInterceptor.setTransactionManager(platformTransactionManager);
         Properties transactionAttributes = new Properties();
 
-        // 新增
-        transactionAttributes.setProperty("insert*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("save*","PROPAGATION_REQUIRED,-Throwable");
-        // 修改
-        transactionAttributes.setProperty("update*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("modify*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("alter*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("cancel*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("refuse*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("enable*","PROPAGATION_REQUIRED,-Throwable");
-        transactionAttributes.setProperty("disable*","PROPAGATION_REQUIRED,-Throwable");
+        String cudOption = "PROPAGATION_REQUIRED,-Throwable";
+        String rOption = "PROPAGATION_SUPPORTS,-Throwable,readOnly";
 
-        // 删除
-        transactionAttributes.setProperty("delete*","PROPAGATION_REQUIRED,-Throwable");
+        // Insert
+        transactionAttributes.setProperty("insert*",cudOption);
+        transactionAttributes.setProperty("save*",cudOption);
+        transactionAttributes.setProperty("add*",cudOption);
+        // Update
+        transactionAttributes.setProperty("update*",cudOption);
+        transactionAttributes.setProperty("modify*",cudOption);
+        transactionAttributes.setProperty("alter*",cudOption);
+        transactionAttributes.setProperty("cancel*",cudOption);
+        transactionAttributes.setProperty("refuse*",cudOption);
+        transactionAttributes.setProperty("enable*",cudOption);
+        transactionAttributes.setProperty("disable*",cudOption);
+        // Delete
+        transactionAttributes.setProperty("delete*",cudOption);
 
-        //查询
-        transactionAttributes.setProperty("find*","PROPAGATION_SUPPORTS,-Throwable,readOnly");
-        transactionAttributes.setProperty("query*","PROPAGATION_SUPPORTS,-Throwable,readOnly");
-        transactionAttributes.setProperty("search*","PROPAGATION_SUPPORTS,-Throwable,readOnly");
-        transactionAttributes.setProperty("*","PROPAGATION_SUPPORTS,-Throwable,readOnly");
+        // Read
+        transactionAttributes.setProperty("find*",rOption);
+        transactionAttributes.setProperty("query*",rOption);
+        transactionAttributes.setProperty("search*",rOption);
+        transactionAttributes.setProperty("get*",rOption);
+        transactionAttributes.setProperty("select*",rOption);
+        transactionAttributes.setProperty("*",rOption);
 
         transactionInterceptor.setTransactionAttributes(transactionAttributes);
+
         return transactionInterceptor;
     }
 
